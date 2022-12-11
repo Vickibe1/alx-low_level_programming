776250732fcf7a48cb84c652376f8f1db94d6517
Vickibe1 <victoriaiberumu@gmail.com>
2022-12-11 11:39:01 -0800

    6task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 /**
- * main - size of various types of computer
+ * main - Entry point
  *
- * Return: 0
+ * Return: Always 0 (Success)
  */
-int main(voi)
+int main(void)
 {
 	char a;
 	int b;
@@ -12,10 +14,10 @@ int main(voi)
 	long long int d;
 	float f;
 
-	printf("Size of a char: %lu byte(s)/n", (unsigned long)size of (a));
-	printf("Size of an int: %lu byte(s)/n", (unsigned long)size of (b));
-	printf("Size of a long int: %lu byte(s)/n", (unsigned long)size of (c));
-	printf("Size of a long long int: %lu byte(s)/n", (unsigned long)size of (d));
-	printf("Size of a float: %lu byte(s)/n", (unsigned long)size of (f));
+	printf("Size of a char: %lu byte(s)/n", sizeof(a));
+	printf("Size of an int: %lu byte(s)/n", sizeof(b));
+	printf("Size of a long int: %lu byte(s)/n", sizeof(c));
+	printf("Size of a long long int: %lu byte(s)/n", sizeof(d));
+	printf("Size of a float: %lu byte(s)/n", sizeof(f));
 	return (0);
 }
