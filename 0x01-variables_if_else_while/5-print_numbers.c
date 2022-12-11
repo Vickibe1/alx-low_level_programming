755a398eb2fdd969741e1aff025af742d0355964
Vickibe1 <victoriaiberumu@gmail.com>
2022-12-11 03:38:59 -0800

    tired

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 /**
- *
  * main - Entry point
  *
  * Return: Always 0 (Success)
@@ -9,9 +8,8 @@
 int main(void)
 {
 	char i;
-	{
-	for (i = '0'; i ='9'; i++);
-	}
+
+	for (i = '0'; i = '9'; i++)
 	{
 		putchar(i);
 	}
