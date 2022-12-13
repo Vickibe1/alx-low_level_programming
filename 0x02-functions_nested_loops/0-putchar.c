4b1fc7f2447bb4ec8424095948d5efff3907df77
Vickibe1 <victoriaiberumu@gmail.com>
2022-12-13 09:19:20 -0800

    my first code

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int result;
+
+	printf("_putchar");
+
+	putchar('\n');
+	return (0);
+}
