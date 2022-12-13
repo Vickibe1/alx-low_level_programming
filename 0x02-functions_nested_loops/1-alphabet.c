ba4ff53e457f45f7a47fcb4fd191c2a186b3f6e7
Vickibe1 <victoriaiberumu@gmail.com>
2022-12-13 14:13:39 -0800

    task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints lowercase alphabets
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+}
