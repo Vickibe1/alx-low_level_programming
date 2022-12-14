ab3beca9fb8534307694a62680794716215067f5
Vickibe1 <victoriaiberumu@gmail.com>
2022-12-13 16:46:01 -0800

    taak7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,11 +8,13 @@
 
 int print_last_digit(int num)
 {
-	int last_digit = num % 10;
+	int last_digit;
+
+	last_digit = (num % 10);
 
 	if (last_digit < 0)
 	{
-		last_digit *= -1;
+		last_digit = (-1 * last_digit);
 	}
 	_putchar(last_digit + '0');
 
