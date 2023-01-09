fd2ca5ed75f35532203bd3150acecd3cb1420e51
Vickibe1 <victoriaiberumu@gmail.com>
2023-01-09 12:09:16 -0800

    task1

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,7 +4,7 @@
  * main - Prints the program name, followed by a new line.
  * @argc: Ckunts the number of arguments supplied to the program.
  * @argv: Array of pointers to the arguments.
- * 
+ *
  * Return: Always o.
  */
 int main(int __attribute__((__unused__)) argc, char *argv[])
