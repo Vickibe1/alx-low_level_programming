4e52460fcf88aabc716dd51eb969f04d18fca152
Vickibe1 <victoriaiberumu@gmail.com>
2023-01-12 12:31:41 -0800

    task3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -4,6 +4,8 @@
  * *array_range - creates an array of integers and stores numbers min to max,
  * both inclusive in the array
  * @min: smallest number to be stored
+ * @max: largest number to be stored
+ *
  * Return: pointer to the newly created array on success,
  * else NULL if min > max or if malloc fails
  */
