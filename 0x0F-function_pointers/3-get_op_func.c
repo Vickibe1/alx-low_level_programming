587a9b62207be36f4f24541122e99cc1785b1b08
Vickibe1 <victoriaiberumu@gmail.com>
2023-01-18 15:23:31 -0800

    task3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -21,11 +21,6 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while (i < 5)
-	{
-		if (*s == *ops[i].op)
-			return (ops[i].f);
-		i++;
-	}
-	return (NULL);
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
+		return (ops[i].f);
 }
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,6 +1,16 @@
+#include "3-calc.h"
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
+
 /**
  * op_add - adds @a and @b
  * @a: input 1
+ * @b: input 2
  *
  * Return: sum of @a and @b
  */
